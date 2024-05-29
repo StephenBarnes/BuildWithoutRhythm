6cf54e8719ab39e742e1ccd29c9b3cefd2b07e14
Stephen Barnes <stephen6174@gmail.com>
2024-05-29 17:40:20 +0200

    First commit, mod works

---
+++ control.lua
@@ -0,0 +1,149 @@
+local lastMessageTick = 0
+local messageWaitTicks = 15 -- Don't show message if a message was already shown within this many ticks ago.
+
+function posEqual(p1, p2)
+	return p1.x == p2.x and p1.y == p2.y
+end
+
+function length(v)
+	return math.floor(math.sqrt(v.x * v.x + v.y * v.y))
+end
+
+function findBlockingEntity(ent1, rad)
+	-- If the given entity completes an arithmetic trio, this returns one of the other entities in that trio.
+	-- If given entity is an endpoint, it always returns the closer of the two other entities.
+	-- If multiple trios, can return any of them.
+	-- If no trio, returns nil.
+	local pos1 = ent1.position
+	local nearbyEnts = ent1.surface.find_entities_filtered {
+		position = pos1,
+		radius = rad / 2,
+			-- Divide by 2, because if there's a 3rd entity within distance rad, then there must be a 2nd entity within rad/2.
+			-- This reduces search radius and also always returns the nearer blocking entity, in the endpoint case.
+		type = ent1.type,
+	}
+	for _, ent2 in pairs(nearbyEnts) do
+		local pos2 = ent2.position
+		if not posEqual(pos2, pos1) then
+			-- There's two cases we need to block.
+			-- The endpoint case: Our new entity is one of the ends of a trio.
+			-- The midpoint case: Our new entity is the center of a trio.
+
+			local pos3EndpointCase = {
+				x = pos2.x * 2 - pos1.x,
+				y = pos2.y * 2 - pos1.y,
+			}
+			local ent3EndpointCase = ent1.surface.find_entities_filtered {
+				position = pos3EndpointCase,
+				type = ent1.type,
+				limit = 1,
+			}
+			if #ent3EndpointCase == 1 then
+				-- For multi-tile entities, find_entities will sometimes return an entity that overlaps with where we searched, but has its center elsewhere.
+				-- We don't want to block in that case, so we need this extra check.
+				if posEqual(ent3EndpointCase[1].position, pos3EndpointCase) then
+					return ent2
+				end
+			end
+
+			local pos3MidpointCase = {
+				x = pos1.x * 2 - pos2.x,
+				y = pos1.y * 2 - pos2.y,
+			}
+			local ent3MidpointCase = ent1.surface.find_entities_filtered {
+				position = pos3MidpointCase,
+				type = ent1.type,
+				limit = 1,
+			}
+			if #ent3MidpointCase == 1 then
+				if posEqual(ent3MidpointCase[1].position, pos3MidpointCase) then
+					return ent2
+				end
+			end
+		end
+	end
+	return nil
+end
+
+function relativePosString(a, b)
+	-- Given positions a, b, makes a string describing the position of b relative to A, like "25 E" meaning 25 blocks east.
+	local d = {x = b.x - a.x, y = b.y - a.y}
+	local distance = length(d)
+
+	local angle = math.atan2(d.y, d.x) -- Returns angle in radians
+		-- Note Factorio is on an old version of Lua so you need math.atan2 instead of math.atan.
+	-- From testing: Down is 1.57, up is -1.57, left is 3.14, right is 0.
+	-- We have 8 compass directions, so each one's range has length 2pi/8.
+	local dir = nil
+	if angle > 2.748 then
+		dir = "W"
+	elseif angle > 1.963 then
+		dir = "SW"
+	elseif angle > 1.178 then
+		dir = "S"
+	elseif angle > 0.393 then
+		dir = "SE"
+	elseif angle > -0.393 then
+		dir = "E"
+	elseif angle > -1.178 then
+		dir = "NE"
+	elseif angle > -1.963 then
+		dir = "N"
+	elseif angle > -2.749 then
+		dir = "NW"
+	else
+		dir = "W"
+	end
+	return distance .. " " .. dir
+end
+
+local settingNames = { -- Maps entity "type" to setting name.
+	["inserter"] = "BuildWithoutRhythm-block-inserters",
+	["assembling-machine"] = "BuildWithoutRhythm-block-assemblers",
+	["electric-pole"] = "BuildWithoutRhythm-block-power-poles",
+	["solar-panel"] = "BuildWithoutRhythm-block-solar-panels",
+	["furnace"] = "BuildWithoutRhythm-block-furnaces",
+}
+
+function maybeBlockPlayerPlacement(event)
+		local settingName = settingNames[event.created_entity.type]
+		if settingName == nil then return end
+		local radius = settings.global[settingName].value
+		if radius == 0 then return end
+
+		local placed = event.created_entity
+		local blockedBy = findBlockingEntity(placed, radius)
+		if blockedBy == nil then return end
+
+		local player = game.get_player(event.player_index)
+		if game.tick > lastMessageTick + messageWaitTicks then
+			lastMessageTick = game.tick
+			local relativePosString = relativePosString(placed.position, blockedBy.position)
+			player.create_local_flying_text {
+				text = {"cant-build-reason.entity-forms-trio", {"entity-name."..blockedBy.name}, relativePosString},
+				create_at_cursor = true,
+				time_to_live = 120,
+			}
+		end
+		player.mine_entity(placed, true) -- "true" says force mining it even if player's inventory is full.
+end
+
+function maybeBlockRobotPlacement(event)
+	local settingName = settingNames[event.created_entity.type]
+	if settingName == nil then return end
+	local radius = settings.global[settingName].value
+	if radius == 0 then return end
+
+	local placed = event.created_entity
+	local blockedBy = findBlockingEntity(placed, radius)
+	if blockedBy == nil then return end
+
+	local surface = placed.surface
+	local pos = placed.position
+	placed.destroy()
+	surface.spill_item_stack(pos, event.stack, nil, event.robot.force)
+		-- Force arg is to mark the spilled item stack for deconstruction by the robot's force.
+end
+
+script.on_event(defines.events.on_built_entity, maybeBlockPlayerPlacement)
+script.on_event(defines.events.on_robot_built_entity, maybeBlockRobotPlacement)
---
+++ settings.lua
@@ -0,0 +1,42 @@
+data:extend({
+    {
+        order = "1",
+        name = "BuildWithoutRhythm-block-inserters",
+        type = "int-setting",
+        setting_type = "runtime-global",
+        default_value = 300,
+        minimum_value = 0,
+    },
+    {
+        order = "2",
+        name = "BuildWithoutRhythm-block-assemblers",
+        type = "int-setting",
+        setting_type = "runtime-global",
+        default_value = 300,
+        minimum_value = 0,
+    },
+    {
+        order = "3",
+        name = "BuildWithoutRhythm-block-power-poles",
+        type = "int-setting",
+        setting_type = "runtime-global",
+        default_value = 300,
+        minimum_value = 0,
+    },
+    {
+        order = "4",
+        name = "BuildWithoutRhythm-block-solar-panels",
+        type = "int-setting",
+        setting_type = "runtime-global",
+        default_value = 300,
+        minimum_value = 0,
+    },
+    {
+        order = "5",
+        name = "BuildWithoutRhythm-block-furnaces",
+        type = "int-setting",
+        setting_type = "runtime-global",
+        default_value = 300,
+        minimum_value = 0,
+    },
+})
